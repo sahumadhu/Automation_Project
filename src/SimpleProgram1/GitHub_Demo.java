f463313e70ddee60385d1ff843db5aea309a297f
sahumadhu <madhushahu8>
2022-09-29 11:49:28 +0530

    added GitHub Demo in simple Program

---
+++ src/SimpleProgram1/GitHub_Demo.java
@@ -0,0 +1,11 @@
+package SimpleProgram1;
+
+public class GitHub_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub Ex.");
+
+	}
+
+}
