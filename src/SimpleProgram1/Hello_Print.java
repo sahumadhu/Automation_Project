45c567bd2df566994c2fdf6dbecbada754addf26
sahumadhu <madhushahu8>
2022-09-29 11:42:11 +0530

    added Hello Program in Semple Project

---
+++ src/SimpleProgram1/Hello_Print.java
@@ -0,0 +1,10 @@
+package SimpleProgram1;
+
+public class Hello_Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Madhu");
+	}
+
+}
